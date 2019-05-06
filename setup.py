290b4ef75e05e34250bf460ab4c5bccd332d7eb2
Gabriel Couture <gacou54@gmail.com>
2019-05-06 11:08:33 -0400

    Update README to allow the installation with pip

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='pyorthanc',
     version='',
-    packages=['pyorthanc'],
+    packages=find_packages(),
     url='https://github.com/gacou54/pyorthanc',
     license='MIT',
     author='Gabriel Couture',
