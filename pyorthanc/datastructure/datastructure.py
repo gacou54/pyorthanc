b34b6106ba2bbd280cb0cce906229f48eff29b47
Gabriel Couture <gacou54@gmail.com>
2019-05-13 18:16:02 -0400

    Minor progress: push to work on another computer

--- pyorthanc/datastructure/datastructure.py
+++ pyorthanc/datastructure/datastructure.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 # author: gabriel couture
+import asyncio
 from typing import List
 
 from pyorthanc.datastructure.tree.patient import Patient
@@ -24,5 +25,10 @@ def build_patient_forest(orthanc: Orthanc) -> List[Patient]:
         List of patient tree representation
     """
     patient_identifiers: List[str] = orthanc.get_patients().json()
+
+    loop = asyncio.get_event_loop()
+    patients = []
+    for patient_identifier in patient_identifiers:
+        pass
 
     return list(map(lambda i: Patient(i, orthanc), patient_identifiers))
