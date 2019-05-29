0c8b281249ba32fe777bc8dbefcdfbe21e57aa7e
Gabriel Couture <gacou54@gmail.com>
2019-05-29 12:21:11 -0400

    Adding first working integration test in the testing ecosystem

--- tests/integration/orthanc/test_orthanc_patient.py
+++ tests/integration/orthanc/test_orthanc_patient.py
@@ -1,7 +1,9 @@
 # coding: utf-8
 # author: gabriel couture
 import unittest
+from typing import List
 
+from pyorthanc import Orthanc
 from tests.integration import setup_server
 
 
@@ -12,15 +14,27 @@ class TestOrthancPatient(unittest.TestCase):
         global orthanc_subprocess
         orthanc_subprocess = setup_server.setup_orthanc_server()
 
-        setup_server.setup_data()
-
     @classmethod
     def tearDownClass(cls) -> None:
-        setup_server.clear_data()
-
         global orthanc_subprocess
         setup_server.stop_orthanc_server_and_remove_data_directory(orthanc_subprocess)
         del orthanc_subprocess
 
-    def test_something(self):
-        self.assertEqual(True, True)
+    def setUp(self) -> None:
+        self.orthanc = Orthanc(setup_server.ORTHANC_URL)
+
+    def tearDown(self) -> None:
+        self.orthanc = None
+        setup_server.clear_data()
+
+    def given_patient_in_orthanc_server(self):
+        setup_server.setup_data()
+
+    def test_givenPatientInOrthanc_whenGettingPatients_thenResultIdAListOfPatientIdentifierStrings(self):
+        self.given_patient_in_orthanc_server()
+
+        result = self.orthanc.get_patients().json()
+
+        self.assertIsInstance(result, list)
+        for patient_identifier in result:
+            self.assertIsInstance(patient_identifier, str)
