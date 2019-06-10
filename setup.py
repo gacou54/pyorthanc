52d561a88f904960835c1bf70d70f710d7b3e0f6
Gabriel Couture <gacou54@gmail.com>
2019-06-10 14:17:02 -0400

    Adding new version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyorthanc',
-    version='0.1.0',
+    version='0.2.0',
     packages=find_packages(),
     url='https://gitlab.physmed.chudequebec.ca/gacou54/pyorthanc',
     license='MIT',
