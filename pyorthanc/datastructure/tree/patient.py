c7169803e8a9ae36a86e818828b42d700a153306
Gabriel Couture <gacou54@gmail.com>
2019-09-16 11:39:54 -0400

    Adding few methods to Patient

--- pyorthanc/datastructure/tree/patient.py
+++ pyorthanc/datastructure/tree/patient.py
@@ -30,8 +30,8 @@ class Patient:
         """
         self.orthanc = orthanc
 
-        self.patient_identifier = patient_identifier
-        self.patient_information = patient_information
+        self.identifier = patient_identifier
+        self.information = patient_information
 
         self.studies: List[Study] = []
 
@@ -43,7 +43,7 @@ class Patient:
         str
             Patient identifier
         """
-        return self.patient_identifier
+        return self.identifier
 
     def get_main_information(self) -> Dict:
         """Get Patient information
@@ -53,12 +53,12 @@ class Patient:
         Dict
             Dictionary of patient main information.
         """
-        if self.patient_information is None:
-            self.patient_information = self.orthanc.get_patient_information(
-                self.patient_identifier
+        if self.information is None:
+            self.information = self.orthanc.get_patient_information(
+                self.identifier
             )
 
-        return self.patient_information
+        return self.information
 
     def get_id(self) -> str:
         """Get patient ID
@@ -90,6 +90,80 @@ class Patient:
         """
         return self.get_main_information()['MainDicomTags']['PatientSex']
 
+    def get_zip(self) -> bytes:
+        """Get the bytes of the zip file
+
+        Get the .zip file.
+
+        Returns
+        -------
+        bytes
+            Bytes of Zip file of the patient.
+
+        Examples
+        --------
+        >>> from pyorthanc import Orthanc
+        >>> from pyorthanc.datastructure import Patient
+        >>> a_patient = Patient(
+        ...     'A_PATIENT_IDENTIFIER',
+        ...     Orthanc('http://localhost:8042')
+        ... )
+        >>> bytes_content = a_patient.get_zip()
+        >>> with open('patient_zip_file_path.zip', 'wb') as file_handler:
+        ...     file_handler.write(bytes_content)
+
+        """
+        return self.orthanc.get_patient_zip(self.identifier)
+
+    def get_patient_module(self) -> Dict:
+        """Get patient module in a simplified version
+
+        The method returns the DICOM patient module
+        (PatientName, PatientID, PatientBirthDate, ...)
+
+        Returns
+        -------
+        Dict
+            DICOM Patient module.
+        """
+        return self.orthanc.get_patient_module_in_simplified_version(
+            self.identifier
+        )
+
+    def is_protected(self) -> bool:
+        """Get if patient is protected against recycling
+
+        Protection against recycling: False means unprotected, True protected.
+
+        Returns
+        -------
+        bool
+            False means unprotected, True means protected.
+        """
+        return self.orthanc.get_if_patient_is_protected(
+            self.identifier
+        )
+
+    def set_to_protected(self):
+        """Set patient to protected state
+
+        Returns
+        -------
+        None
+            Nothing.
+        """
+        self.orthanc.set_patient_to_protected(self.identifier)
+
+    def set_to_unprotected(self):
+        """Set patient to unprotected state
+
+        Returns
+        -------
+        None
+            Nothing.
+        """
+        self.orthanc.set_patient_to_not_protected(self.identifier)
+
     def get_studies(self) -> List[Study]:
         """Get patient's studies
 
@@ -104,13 +178,28 @@ class Patient:
         """Build a list of the patient's studies
         """
         studies_information = self.orthanc.get_patient_studies_information(
-            self.patient_identifier
+            self.identifier
         )
 
         self.studies = list(map(
             lambda i: Study(i['ID'], self.orthanc),
             studies_information
         ))
+
+    def anonymize(self) -> 'Patient':
+        """Anonymize patient
+
+        If no error is been raise, then it creates a new anonymous patient.
+        Documentation: http://book.pyorthanc-server.com/users/anonymization.html
+
+        Returns
+        -------
+        Patient
+            A New anonymous patient.
+        """
+        new_anonymous_patient = self.orthanc.anonymize_patient(self.identifier)
+
+        return Patient(new_anonymous_patient['ID'], self.orthanc)
 
     def __str__(self):
         return f'Patient (id={self.get_id()}, identifier={self.get_identifier()})'
