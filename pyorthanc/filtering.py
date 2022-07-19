1327a83df3bcec3a964e6a3ce6726f6a0a8d825b
Gabriel Couture <gacou54@gmail.com>
2022-07-19 11:33:15 -0400

    Functionnal Instance

--- pyorthanc/filtering.py
+++ pyorthanc/filtering.py
@@ -1,10 +1,22 @@
 import os
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from typing import List, Dict, Callable, Optional
 
 from pyorthanc import Study, Series, Instance
 from pyorthanc.client import Orthanc
 from pyorthanc.patient import Patient
+
+
+def make_datetime_from_dicom_date(date: str, time: str) -> datetime:
+    return datetime(
+        year=int(date[:4]),
+        month=int(date[4:6]),
+        day=int(date[6:8]),
+        hour=int(time[:2]),
+        minute=int(time[2:4]),
+        second=int(time[4:6])
+    )
 
 
 def build_patient_forest(
@@ -113,7 +125,7 @@ def _build_series(
         if not series_filter(series):
             return series
 
-    series.instances = [Instance(i['ID'], orthanc, i) for i in instance_information]
+    series._instances = [Instance(i['ID'], orthanc, i) for i in instance_information]
 
     return series
 
