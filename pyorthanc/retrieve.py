c156f2fe15866feadd9ad0f23feffcf3824a9276
gacou54 <gacou54@gmail.com>
2023-08-29 11:21:12 -0400

    Simplify the retrieve_an_write functions by changing path
    Now PatientID/StudyInstanceUID/SeriesInstanceUID/SOPInstanceUID.dcm

--- pyorthanc/retrieve.py
+++ pyorthanc/retrieve.py
@@ -17,29 +17,26 @@ def retrieve_and_write_patients(patients: List[Patient], path: str) -> None:
     path
         Path where you want to write the files.
     """
-    os.makedirs(path, exist_ok=True)
     for patient in patients:
         retrieve_and_write_patient(patient, path)
 
 
 def retrieve_and_write_patient(patient: Patient, path: str) -> None:
-    patient_path = _make_patient_path(path, patient.patient_id)
-    os.makedirs(patient_path, exist_ok=True)
+    patient_path = os.path.join(path, patient.patient_id)
 
     for study in patient.studies:
         retrieve_and_write_study(study, patient_path)
 
 
 def retrieve_and_write_study(study: Study, patient_path: str) -> None:
-    study_path = _make_study_path(patient_path, study.uid)
-    os.makedirs(study_path, exist_ok=True)
+    study_path = os.path.join(patient_path, study.uid)
 
     for series in study.series:
         retrieve_and_write_series(series, study_path)
 
 
 def retrieve_and_write_series(series: Series, study_path: str) -> None:
-    series_path = _make_series_path(study_path, series.modality)
+    series_path = os.path.join(study_path, series.uid)
     os.makedirs(series_path, exist_ok=True)
 
     for instance in series.instances:
@@ -53,36 +50,3 @@ def retrieve_and_write_instance(instance: Instance, series_path) -> None:
 
     with open(path, 'wb') as file_handler:
         file_handler.write(dicom_file_bytes)
-
-
-def _make_patient_path(path: str, patient_id: str) -> str:
-    patient_directories = os.listdir(path)
-
-    if patient_id.strip() == '':
-        patient_id = 'anonymous-patient'
-        return os.path.join(path, _make_path_name(patient_id, patient_directories))
-
-    return os.path.join(path, patient_id)
-
-
-def _make_study_path(patient_path: str, study_uid: str) -> str:
-    return os.path.join(patient_path, study_uid)
-
-
-def _make_series_path(study_path: str, modality: str) -> str:
-    series_directories = os.listdir(study_path)
-
-    return os.path.join(study_path, _make_path_name(modality, series_directories))
-
-
-def _make_path_name(name: str, directories: List[str], increment: int = 1, has_increment: bool = False) -> str:
-    if not has_increment:
-        name = f'{name}-{increment}'
-        has_increment = True
-    else:
-        name = '-'.join(name.split('-')[:-1])
-
-    if name in directories:
-        return _make_path_name(name, directories, increment + 1, has_increment)
-
-    return name
--- tests/test_retrieve.py
+++ tests/test_retrieve.py
@@ -18,7 +18,7 @@ def path():
 def test_retrieve_and_write_patients(patient, study, series, instance, path):
     retrieve_and_write_patients([patient], path)
 
-    assert f'{path}/{patient.patient_id}/{study.uid}/{series.modality}-1/{instance.uid}.dcm' in glob.glob(
+    assert f'{path}/{patient.patient_id}/{study.uid}/{series.uid}/{instance.uid}.dcm' in glob.glob(
         f'{path}/**/*.dcm',
         recursive=True
     )
