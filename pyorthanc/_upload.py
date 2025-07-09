605eafcc219597220b1a008595ad73d7ce209e72
Yannick Lemaréchal <10884038+ylemarechal@users.noreply.github.com>
2025-07-09 10:36:24 -0400

    ✨ Add check_before_upload option
    # Conflicts:
    #	pyorthanc/_upload.py

--- pyorthanc/_upload.py
+++ pyorthanc/_upload.py
@@ -8,9 +8,10 @@ import httpx
 import pydicom
 
 from pyorthanc import AsyncOrthanc, Orthanc
+from pyorthanc.util import _make_orthanc_id, to_orthanc_study_id, to_orthanc_series_id, to_orthanc_patient_id
 
 
-def upload(client: Orthanc, path_or_ds: Union[str, Path, pydicom.Dataset], recursive: bool = False) -> Union[Dict, httpx.Response]:
+def upload(client: Orthanc, path_or_ds: Union[str, Path, pydicom.Dataset], recursive: bool = False, check_before_upload: bool = False) -> Union[Dict, httpx.Response]:
     """Upload a DICOM file or dataset to Orthanc synchronously
 
     Parameters
@@ -21,6 +22,8 @@ def upload(client: Orthanc, path_or_ds: Union[str, Path, pydicom.Dataset], recur
         Either a path to a DICOM file, directory, zip file or a pydicom Dataset object
     recursive : bool
         When `path_or_ds` is a directory, whether to upload recursively all the DICOM files in the directory
+    check_before_upload : bool
+         Verify if data is already in Orthanc before sending it. It verifies if a file is stored, there is no file comparison.
     """
     if (isinstance(path_or_ds, str) or isinstance(path_or_ds, Path)) and os.path.isdir(path_or_ds):
         for dicom_bytes in _prepare_data_directory(path_or_ds, recursive=recursive):
@@ -28,6 +31,11 @@ def upload(client: Orthanc, path_or_ds: Union[str, Path, pydicom.Dataset], recur
 
     else:
         dicom_bytes = _prepare_data_from_ds_or_file(path_or_ds)
+
+        if check_before_upload:
+            data_in_orthanc, return_message = _is_data_already_in_orthanc(client, dicom_bytes)
+            if data_in_orthanc:
+                return return_message
 
         return client.post_instances(dicom_bytes)
 
@@ -78,3 +86,34 @@ def _prepare_data_directory(directory: str, recursive: bool) -> Generator[bytes,
 
     for filepath in filepaths:
         yield _prepare_data_from_ds_or_file(filepath)
+
+
+def _is_data_already_in_orthanc(client: Orthanc, dicom_bytes):
+    dicom_file_like = BytesIO(dicom_bytes)
+    ds = pydicom.dcmread(dicom_file_like)
+    orthanc_id = _make_orthanc_id_from_ds(ds)
+
+    try:
+        info = client.get_instances_id_metadata(id_=orthanc_id)
+
+        return True, _make_already_stored_return_message(ds)
+    except httpx.HTTPError as e:
+        return False, ''
+
+
+def _make_orthanc_id_from_ds(ds: pydicom.Dataset):
+    return _make_orthanc_id(patient_id=ds.PatientID, study_uid=ds.StudyInstanceUID, series_uid=ds.SeriesInstanceUID,
+                            instance_uid=ds.SOPInstanceUID)
+
+
+def _make_already_stored_return_message(ds):
+    instance_id = _make_orthanc_id_from_ds(ds)
+
+    data = {'ID': instance_id,
+            'ParentPatient': to_orthanc_patient_id(ds.PatientID),
+            'ParentSeries': to_orthanc_series_id(ds.PatientID, ds.StudyInstanceUID, ds.SeriesInstanceUID),
+            'ParentStudy': to_orthanc_study_id(ds.PatientID, ds.StudyInstanceUID),
+            'Path': f'/instances/{instance_id}',
+            'Status': 'AlreadyStored'}
+
+    return data
